f2a6703d41b211cfb51167a1cb4ac4157a4153d5
o.olea <o.olea@sintraconsulting.eu>
2021-12-27 08:42:17 +0100

    vuex 1 getters

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Create Event,  </h1>
+    <h2>{{ user.name }}</h2>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <h3 >{{ catLength }}</h3>
+    <h4>{{ getEvent(1) }}</h4>
+    <h5>{{ getEventById(1) }}</h5>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    getEvent() {
+      return this.$store.getters.getEventById
+    },
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
